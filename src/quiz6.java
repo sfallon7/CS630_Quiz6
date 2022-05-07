1a98cd0fc78df14e2c613188d92d8b19b051c867
Sean Fallon <sfallon7@gmail.com>
2022-05-07 11:23:45 -0400

    New Project

---
+++ src/quiz6.java
@@ -0,0 +1,26 @@
+public class quiz6 {
+    class Process {
+        int pid;
+        int priority;
+        int burstTime;
+
+        public Process(int pid, int priority, int burstTime) {
+            this.pid = pid;
+            this.priority = priority;
+            this.burstTime = burstTime;
+        }
+    }
+
+    public static void main(String[] args) {
+    //Main Function
+    }
+
+    public void sortPriority(Process proc[]) {
+    }
+
+    public static void drawGantChart(Process proc[]) {
+        System.out.println(--);
+
+
+    }
+}
