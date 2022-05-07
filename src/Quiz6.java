d2d18e372bb543713e50526cac8beaf5f8405b0d
gloriankosi <kosig@acm.org>
2022-05-07 14:32:16 -0400

    ordering

--- src/Quiz6.java
+++ src/Quiz6.java
@@ -3,50 +3,46 @@ import java.util.PriorityQueue;
 
 public class Quiz6 {
 
-
-    public static void main(String[] args) {
-        //Main Function
-        int tick;
-        int totalTime;
-
-        //Instantiate 3 Processes
-        Process proc1A = new Process("pA", 2, 10, 1);
-        Process proc2B = new Process("pB", 3, 7, 2);
-
-        //Instantiate Priority Queue
-        PriorityQueue<Process> priQueue = new PriorityQueue<Process>();
-
-        //Add Processes to PriorityQue;
-        priQueue.add(proc1A);
-        priQueue.add(proc2B);
-
-        System.out.println(priQueue.peek());
-    }
-
-    static class Process {
+    static class Process{
         String pid;
         int priority;
         int burstTime;
-
         int pNum;
 
-        public Process(String pid, int priority, int burstTime, int pNum) {
+        public Process(String pid, int priority, int burstTime, int pNum){
             this.pid = pid;
             this.priority = priority;
             this.burstTime = burstTime;
             this.pNum = pNum;
         }
     }
+    static class ProcessComparator implements Comparator<Process> {
 
-    class ProcessComparator implements Comparator<Process> {
-        public int compare(Process p1, Process p2) {
-            if (p1.priority > p2.priority)
+        @Override
+        public int compare(Quiz6.Process o1, Quiz6.Process o2) {
+            if(o1.priority < o2.priority) {
                 return 1;
-            else if (p1.priority < p2.priority)
+            }
+            if(o1.priority > o2.priority) {
                 return -1;
-            else if (p1.priority == p2.priority)
-                return 0;
+            }
             return 0;
         }
+
+    }
+    public static void main(String[] args) {
+        int tick;
+        int totalTime;
+        // Instantiate 3 Processes
+        Process proc1A = new Process("pA", 10, 10, 1);
+        Process proc2B = new Process("pB", 3, 7, 2);
+        Process proc1B = new Process("pR", 7, 2, 3);
+
+        // Instantiate Priority Queue
+        PriorityQueue<Process> priQueue = new PriorityQueue<Process>(1 , new ProcessComparator());
+        priQueue.add(proc1A);
+        priQueue.add(proc2B);
+        priQueue.add(proc1B);
+        System.out.println(priQueue.peek().pid);
     }
 }
